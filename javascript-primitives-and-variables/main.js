853516c364722c389f005a11411e289331f6ab12
Alexander <amchops@comcast.net>
2021-12-06 10:50:09 -0800

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,29 @@
+var fullName = 'Alexander Davis';
+
+var isCool = true;
+
+var totalPets = 3;
+
+console.log('value of fullName:', fullName);
+
+console.log('is cool:', isCool);
+
+console.log('total pets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+
+console.log('typeof isCool:', typeof isCool);
+
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+
+console.log('uknown', unknown);
+
+console.log('typeof uknown:', typeof unknown);
+
+var never;
+
+console.log('value of never:', never);
+
+console.log('typeof never:', typeof never);
