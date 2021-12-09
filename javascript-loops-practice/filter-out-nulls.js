8dbf80df95298883d4cae482a9c73a3daf32e317
Alexander <amchops@comcast.net>
2021-12-08 22:08:17 -0800

    Completed javascript-loops-practice.

---
+++ javascript-loops-practice/filter-out-nulls.js
@@ -0,0 +1,8 @@
+/* exported filterOutNulls */
+
+function filterOutNulls(values) {
+  for (var i = 0; i <= 0; i++) {
+    var array = values.filter(values => values != null);
+  }
+  return array;
+}
---
+++ javascript-loops-practice/filter-out-strings.js
@@ -0,0 +1,8 @@
+/* exported filterOutStrings */
+
+function filterOutStrings(values) {
+  for (var i = 0; i <= 0; i++) {
+    var array = values.filter(values => values <= 98);
+  }
+  return array;
+}
