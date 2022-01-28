62b0352561f7e4822a9644b66588fe506f5c41eb
Alex-Davis0 <amchops@comcast.net>
2022-01-28 11:09:32 -0800

    Completed sql-aggregates

--- sql-aggregates/starring-in.sql
+++ sql-aggregates/starring-in.sql
@@ -5,5 +5,6 @@ from "films" as "f"
 join "filmGenre" as "fg" using ("filmId")
 join "genres" as "g" using ("genreId")
 join "castMembers" as "c" using ("filmId")
-where "actorId" = 178
+join "actors" as "a" using ("actorId")
+where "firstName" = 'Lisa' and "lastName" = 'Monroe'
 group by "genreId"
