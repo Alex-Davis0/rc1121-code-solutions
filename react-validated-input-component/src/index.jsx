73122ef27225fab3f4281f1aaf2c3bd6955bdc67
Alex-Davis0 <amchops@comcast.net>
2022-02-04 15:21:57 -0800

    Completed react-validated-input-component

---
+++ react-validated-input-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Validated from './validated-input';
+
+ReactDOM.render(
+  <Validated />,
+  document.querySelector('#root')
+);
